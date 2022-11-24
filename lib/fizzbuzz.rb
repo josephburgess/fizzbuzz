d835303158fc940e495fc5cd34a3789990b75d0f
Finn McCoy <finnmccoy99@gmail.com>
2022-11-24 16:09:39 +0000

    Working fizzbuzz created

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  "fizz"
+  div3 = number % 3 == 0
+  div5 = number % 5 == 0
+  if div3 && div 5
+    "fizzbuzz"
+  elsif div3
+    "fizz"
+  elsif div5
+    "buzz"
+  else
+    "#{number}"
 end
