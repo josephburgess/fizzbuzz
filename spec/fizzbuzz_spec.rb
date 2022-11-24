15ba12fb4e71633f05a9313b8ccc45e05b0eab75
Joe Burgess <josephburgess@gmail.com>
2022-11-24 16:03:30 +0000

    adding further tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require "fizzbuzz"
 
 describe "fizzbuzz" do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiple of 3' do
     expect(fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
   end
 end
